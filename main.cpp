8fc93d395eb713d918a5df17a68c2bd7e6a778f4
fusto <e.tscharntke@ostfalia.de>
2022-05-05 23:56:55 +0200

    erster

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+//###//
+using namespace std;
+using namespace sf;
+//###//
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
     return 0;
 }
