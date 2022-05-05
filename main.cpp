ac1d21e31f7081f0d029446fd75787f75ed93a72
yrrrmmbl <n0op@protonmail.com>
2022-05-05 23:51:44 +0200

    changed main.ccp so that it multithreads

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <thread>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+void thread_function()
+{
+	std::cout << std::this_thread::get_id() << "Goodbye, cruel world!" << std::endl;
+}
+
+int main()
+{
+	std::cout << "Hello, World!" << std::endl;
+	std::thread thread(&thread_function);
+	thread.join();
+	return 0;
 }
