323d94572b787669dc4a6053c8b03f1dac85a022
titison <philipp@titusgames.de>
2022-05-06 22:26:45 +0200

    MouseDrag

--- main.cpp
+++ main.cpp
@@ -17,6 +17,8 @@ int main()
     Renderer renderer = Renderer(map);
     CameraContext context = CameraContext{zoom : 2.0f,camera_pos : Vector2f(15.0f,15.0f)};
     Vector2f movement = {0.0f,0.0f};
+    Vector2f oldMouse = {0.0f,0.0f};
+    bool mouseButtonPressed = false;
 	sf::Event event{};
 
 	while (window.isOpen())
@@ -55,6 +57,23 @@ int main()
                 }
                 else if(event.key.code == sf::Keyboard::Down) {
                     movement.y += 0.1f;
+                }
+            }
+            if (event.type == sf::Event::MouseButtonPressed) {
+                mouseButtonPressed = true;
+            }
+            if (event.type == sf::Event::MouseButtonReleased) {
+                mouseButtonPressed = false;
+            }
+            if (event.type == sf::Event::MouseMoved) {
+                if(mouseButtonPressed) {
+                    context.camera_pos.x -= oldMouse.x - event.mouseMove.x;
+                    context.camera_pos.y -= oldMouse.y - event.mouseMove.y;
+                    oldMouse.x = event.mouseMove.x;
+                    oldMouse.y = event.mouseMove.y;
+                } else {
+                    oldMouse.x = event.mouseMove.x;
+                    oldMouse.y = event.mouseMove.y;
                 }
             }
             if(movement.x > 0.1f) movement.x = 0.1f;
