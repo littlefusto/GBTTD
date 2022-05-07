3eac5369e426957fd60599c46600499543a20767
yrrrmmbl <n0op@protonmail.com>
2022-05-07 17:57:29 +0200

    added gamestates

---
+++ 00_Game/Game.h
@@ -0,0 +1,10 @@
+//
+// Created by noop on 07.05.2022.
+//
+#pragma once
+enum game_state
+{
+	state_menu = 0,
+	state_map = 1
+};
+//GBTTD_GAME_H
--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <Textures.h>
 #include <Map.h>
 #include "01_Graphics/Renderer.h"
+#include <Game.h>
 //###//
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wgnu-designator"
@@ -20,102 +21,124 @@ int main()
 	Vector2f movement = { 0.0f, 0.0f };
 	Vector2f oldMouse = { 0.0f, 0.0f };
 	bool RMBPressed = false;
+	game_state gameState = state_menu;
 	sf::Event event{};
 
 	while (window.isOpen())
 	{
 		while (window.pollEvent(event))
 		{
-			switch (event.type)
+			switch (gameState)
 			{
-			case sf::Event::Closed:
-				window.close();
-				break;
-			case sf::Event::MouseWheelScrolled:
-				if (event.mouseWheelScroll.delta > 0.0f)
+			case state_menu:
+				switch (event.type)
 				{
-					context.zoom *= 1.1;
-				} else
-				{
-					context.zoom /= 1.1;
-				}
-				break;
-			case sf::Event::KeyPressed:
-				switch (event.key.code)
-				{
-				case sf::Keyboard::Left:
-					movement.x += 0.1f;
+				case sf::Event::KeyPressed:
+					if (event.key.code == sf::Keyboard::Escape) { gameState = state_map; }
 					break;
-				case sf::Keyboard::Right:
-					movement.x += -0.1f;
-					break;
-				case sf::Keyboard::Up:
-					movement.y += 0.1f;
-					break;
-				case sf::Keyboard::Down:
-					movement.y += -0.1f;
+				case sf::Event::Closed:
+					window.close();
 					break;
 				default:
 					break;
 				}
 				break;
-			case sf::Event::KeyReleased:
-				switch (event.key.code)
+			case state_map:
+				switch (event.type)
 				{
-				case sf::Keyboard::Left:
-					movement.x += -0.1f;
+				case sf::Event::Closed:
+					window.close();
 					break;
-				case sf::Keyboard::Right:
-					movement.x += 0.1f;
+				case sf::Event::MouseWheelScrolled:
+					if (event.mouseWheelScroll.delta > 0.0f)
+					{
+						context.zoom *= 1.1;
+					} else
+					{
+						context.zoom /= 1.1;
+					}
 					break;
-				case sf::Keyboard::Up:
-					movement.y += -0.1f;
+				case sf::Event::KeyPressed:
+					switch (event.key.code)
+					{
+					case sf::Keyboard::Left:
+						movement.x += 0.1f;
+						break;
+					case sf::Keyboard::Right:
+						movement.x += -0.1f;
+						break;
+					case sf::Keyboard::Up:
+						movement.y += 0.1f;
+						break;
+					case sf::Keyboard::Down:
+						movement.y += -0.1f;
+						break;
+					case sf::Keyboard::Escape:
+						gameState = state_menu;
+						break;
+					default:
+						break;
+					}
 					break;
-				case sf::Keyboard::Down:
-					movement.y += 0.1f;
+				case sf::Event::KeyReleased:
+					switch (event.key.code)
+					{
+					case sf::Keyboard::Left:
+						movement.x += -0.1f;
+						break;
+					case sf::Keyboard::Right:
+						movement.x += 0.1f;
+						break;
+					case sf::Keyboard::Up:
+						movement.y += -0.1f;
+						break;
+					case sf::Keyboard::Down:
+						movement.y += 0.1f;
+						break;
+					default:
+						break;
+					}
+					break;
+				case Event::MouseButtonPressed:
+					if (event.mouseButton.button == sf::Mouse::Right)
+					{
+						RMBPressed = true;
+					} else if (event.mouseButton.button == sf::Mouse::Left)
+					{
+						sf::Vector2i pos;
+						pos.x = event.mouseButton.x;
+						pos.y = event.mouseButton.y;
+						renderer.getClickedTile(pos, context);
+					}
+					break;
+				case Event::MouseButtonReleased:
+					if (event.mouseButton.button == sf::Mouse::Right)
+					{
+						RMBPressed = false;
+					}
+					break;
+				case Event::MouseMoved:
+					if (RMBPressed)
+					{
+						context.camera_pos.x -= oldMouse.x - event.mouseMove.x;
+						context.camera_pos.y -= oldMouse.y - event.mouseMove.y;
+						oldMouse.x = event.mouseMove.x;
+						oldMouse.y = event.mouseMove.y;
+					} else
+					{
+						oldMouse.x = event.mouseMove.x;
+						oldMouse.y = event.mouseMove.y;
+					}
 					break;
 				default:
 					break;
 				}
-				break;
-			case Event::MouseButtonPressed:
-				if (event.mouseButton.button == sf::Mouse::Right)
-				{
-					RMBPressed = true;
-				} else if (event.mouseButton.button == sf::Mouse::Left)
-				{
-					sf::Vector2i pos;
-					pos.x = event.mouseButton.x;
-					pos.y = event.mouseButton.y;
-					renderer.getClickedTile(pos, context);
-				}
-				break;
-			case Event::MouseButtonReleased:
-				if (event.mouseButton.button == sf::Mouse::Right)
-				{
-					RMBPressed = false;
-				}
-				break;
-			case Event::MouseMoved:
-				if (RMBPressed)
-				{
-					context.camera_pos.x -= oldMouse.x - event.mouseMove.x;
-					context.camera_pos.y -= oldMouse.y - event.mouseMove.y;
-					oldMouse.x = event.mouseMove.x;
-					oldMouse.y = event.mouseMove.y;
-				} else
-				{
-					oldMouse.x = event.mouseMove.x;
-					oldMouse.y = event.mouseMove.y;
-				}
-				break;
-			default:
+				if (movement.x > 0.1f) movement.x = 0.1f;
+				if (movement.x < -0.1f) movement.x = -0.1f;
+				if (movement.y > 0.1f) movement.y = 0.1f;
+				if (movement.y < -0.1f) movement.y = -0.1f;
 				break;
 			}
-			if (movement.x > 0.1f) movement.x = 0.1f;
-			if (movement.x < -0.1f) movement.x = -0.1f;
-			if (movement.y > 0.1f) movement.y = 0.1f;
-			if (movement.y < -0.1f) movement.y = -0.1f;
 		}
 		context.camera_pos.x += movement.x;
 		context.camera_pos.y += movement.y;
