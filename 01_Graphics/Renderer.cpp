9143516023e79249a5a1752bb32a3d1df75c13ec
yrrrmmbl <n0op@protonmail.com>
2022-05-07 22:51:59 +0200

    formatting

--- 01_Graphics/Renderer.cpp
+++ 01_Graphics/Renderer.cpp
@@ -29,8 +29,8 @@ bool Renderer::generateMap()
 			int x = (TILE_WIDTH / 2 * i) - (TILE_WIDTH / 2 * j) + ((MAP_SIZE * TILE_WIDTH) / 2) -
 				(TILE_WIDTH / 2);
 			int y = (TILE_HEIGTH / 2 * i) + (TILE_HEIGTH / 2 * j);
-			map_image.copy(content[j][i]->getTileType()->getTileImage(), x, y, sf::IntRect(0, 0, 0, 0),
-				       true);
+			map_image.copy(content[j][i]->getTileType()->getTileImage(), x, y,
+				       sf::IntRect(0, 0, 0, 0), true);
 		}
 	}
 	if (selected_tile.x >= 0)
