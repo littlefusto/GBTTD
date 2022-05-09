abca9320b3232edaea17215fb2827253256cf502
titison <philipp@titusgames.de>
2022-05-09 16:55:18 +0200

    JSON MapLoader uses strings now

--- 03_Saves/JSONMapLoader.cpp
+++ 03_Saves/JSONMapLoader.cpp
@@ -4,6 +4,70 @@
 
 #include "JSONMapLoader.h"
 
+std::string slopeToString(Slope slope) {
+	std::string slope_string = "";
+	if(slope == FLAT){
+		slope_string += "flat";
+	}else if(slope & STEEP){
+		slope_string += "steep_";
+	}
+	if(slope & N){
+		slope_string += "N";
+	}
+	if(slope & E){
+		slope_string += "E";
+	}
+	if(slope & S){
+		slope_string += "S";
+	}
+	if(slope & W){
+		slope_string += "W";
+	}
+	return slope_string;
+}
+
+Slope stringToSlope(const std::string& slope_string) {
+	unsigned char slope = FLAT;
+	if(slope_string.find("steep") != std::string::npos) {
+		slope |= STEEP;
+		slope_string.substr(5,slope_string.length()-5);
+	}
+	if(slope_string.find('N') != std::string::npos) {
+		slope |= N;
+	}
+	if(slope_string.find('E') != std::string::npos) {
+		slope |= E;
+	}
+	if(slope_string.find('S') != std::string::npos) {
+		slope |= S;
+	}
+	if(slope_string.find('W') != std::string::npos) {
+		slope |= W;
+	}
+	return (Slope) slope;
+}
+
+std::string tileTypeToString(TileType tileType) {
+	std::string tile_type_string = "";
+	if(tileType == DEFAULT){
+		tile_type_string += "default";
+	}else if(tileType == GRASS){
+		tile_type_string += "grass";
+	}else{
+		tile_type_string += "default";
+	}
+	return tile_type_string;
+}
+
+TileType stringToTileType(const std::string& tile_type_string) {
+	TileType tileType = DEFAULT;
+	if( tile_type_string == "default"){
+		tileType = DEFAULT;
+	}else if(tile_type_string == "grass"){
+		tileType = GRASS;
+	}
+}
+
 void JSONMapLoader::saveMap(Map &map, std::string& path)
 {
 	vector<vector<Tile*>> &content = map.getContent();
@@ -17,8 +81,8 @@ void JSONMapLoader::saveMap(Map &map, std::string& path)
 	{
 		for (int h = 0; h < mapYsize; h++)
 		{
-			savefile["Tiles"][w][h]["Type"] = content[h][w]->getTileType();
-			savefile["Tiles"][w][h]["Slope"] = content[h][w]->getTileSlope();
+			savefile["Tiles"][w][h]["Type"] = tileTypeToString(content[h][w]->getTileType());
+			savefile["Tiles"][w][h]["Slope"] = slopeToString(content[h][w]->getTileSlope());
 			savefile["Tiles"][w][h]["Height"] = content[h][w]->getHeight();
 		}
 	}
@@ -39,8 +103,8 @@ Map* JSONMapLoader::loadMap(std::string& path) {
 	{
 		for (int h = 0; h < mapYsize; h++)
 		{
-			TileType tile_type = (TileType) savefile["Tiles"][w][h]["Type"].asInt();
-			Slope slope = (Slope) savefile["Tiles"][w][h]["Slope"].asInt();
+			TileType tile_type = stringToTileType(savefile["Tiles"][w][h]["Type"].asString());
+			Slope slope = stringToSlope(savefile["Tiles"][w][h]["Slope"].asString());
 			int height = savefile["Tiles"][w][h]["Height"].asInt();
 			map->getContent()[h][w] = new Tile(height, tile_type, slope);
 		}
